ba3b94975eb8fce796fe4157c9258eb8f8b18f3f
Ben Nugent <bsnugent@gmail.com>
2013-01-08 10:25:39 -0500

    Initial commit

---
+++ user-files/simulations/magento/Headers.scala
@@ -0,0 +1,169 @@
+package magento
+
+object Headers {
+
+  val headers_1 = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate"""
+  )
+
+  val headers_2 = Map(
+      "Accept" -> """*/*"""
+  )
+
+  val headers_3 = Map(
+      "Accept" -> """text/css,*/*;q=0.1"""
+  )
+
+  val headers_73 = Map(
+      "Accept" -> """text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8""",
+      "Content-Type" -> """application/x-www-form-urlencoded"""
+  )
+
+  val headers_checkout_onepage = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/cart/"""
+  )
+
+  val headers_savemethod = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/""",
+    "Content-Length" -> """12"""
+  )
+
+  val headers_getbilling = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/"""
+  )
+
+  val headers_savebilling = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/""",
+    "Content-Length" -> """523"""
+  )
+
+  val headers_getadditional = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/""",
+    "Content-Length" -> """0"""
+  )
+
+  val headers_getshipping = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/"""
+  )
+
+  val headers_saveshipping = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/""",
+    "Content-Length" -> """108"""
+  )
+
+  val headers_getpayment = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/"""
+  )
+
+  val headers_savepayment = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/""",
+    "Content-Length" -> """27"""
+  )
+
+  val headers_getreview = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/"""
+  )
+
+  val headers_savereview = Map(
+    "Host" -> """mce000-lb.nexcess.net""",
+    "User-Agent" -> """Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0""",
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Accept-Language" -> """en-US,en;q=0.5""",
+    "Accept-Encoding" -> """gzip, deflate""",
+    "X-Requested-With" -> """XMLHttpRequest""",
+    "X-Prototype-Version" -> """1.7""",
+    "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+    "Referer" -> """http://mce000-lb.nexcess.net/checkout/onepage/""",
+    "Content-Length" -> """27"""
+  )
+
+	val headers_1a = Map(
+			"Accept" -> """text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"""
+	)
+
+	val headers_2a = Map(
+			"Accept" -> """text/css,*/*;q=0.1"""
+	)
+
+	val headers_3a = Map(
+			"Accept" -> """*/*"""
+	)
+
+}
---
+++ user-files/simulations/magento/MagentoSimulation-300c_3000b.scala
@@ -0,0 +1,21 @@
+
+package magento
+import com.excilys.ebi.gatling.core.Predef._
+import com.excilys.ebi.gatling.http.Predef._
+import com.excilys.ebi.gatling.jdbc.Predef._
+import com.excilys.ebi.gatling.http.Headers.Names._
+import akka.util.duration._
+import bootstrap._
+import assertions._
+
+class MagentoSimulation300 extends Simulation {
+  val urlBase = "http://mce000-lb.nexcess.net"
+  val httpConf = httpConfig
+      .baseURL(urlBase)
+      .acceptEncodingHeader("gzip, deflate")
+      .userAgentHeader("Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0")
+
+  setUp(AnonymousCheckout.scn.users(300).protocolConfig(httpConf),
+        AnonymousBrowser.scn2.users(3000).protocolConfig(httpConf))
+
+}
